ba51bb30755743431237f3471618857495536359
meashishpokhrel <meashishpokhrel@gmail.com>
2020-03-17 13:11:03 +0545

    INput Output system call

--- iocall.c
+++ iocall.c
@@ -4,4 +4,16 @@
 int main(){
 int fd;
 char buffer[80];
-char msg[50]
+char msg[50]="Ashish Pokhrel";
+fd=open("check.txt",O_RDWR);
+printf("fd=%d",fd);
+if (fd!=-1){
+printf("\n check.txt opened with read write access \n");
+write (fd,msg,sizeof(msg));
+lseek(fd,0,SEEK_SET);
+read(fd,buffer,sizeof(msg));
+printf("\n %s was written to the file \n",buffer);
+close(fd);
+}
+return 0;
+}
