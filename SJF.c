47986193b7fa53335dbfba99382b9c1b275c15f3
meashishpokhrel <meashishpokhrel@gmail.com>
2020-03-17 18:08:15 +0545

    SJF Algorithm

---
+++ SJF.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <conio.h>
+#define max 30
+
+void main(){
+    int i,j,n,t,p[max],bt[max],wt[max],tat[max];
+    float awt=0, atat=0;
+    //clrscr();
+    printf("enter the total number of proccess");
+    scanf("%d",&n);
+    printf("enter the process number");
+    
+    for (i=0; i<n; i++){
+        scanf("%d", &p[i]);
+    }
+    printf("enter the burst time");
+    
+    for (i=0; i<n; i++){
+        scanf("%d",&bt[i]);
+    }
+    //Applyng bubble sort to the burst time
+    
+    for (i=0; i<n; i++){
+        for(j=0; j<n-i-1; j++){
+            if (bt[j]>bt[j+1]){
+                t=bt[j];
+                bt[j]=bt[j+1];
+                bt[j+1]=t;
+                
+                t=p[j];
+                p[j]=p[j+1];
+                p[j+1]=t;
+            }
+        }
+    }
+    
+    printf("process\t burst time\t waiting time\t turn around time\n");
+    for (i=0; i<n; i++){
+        wt[i]=0;
+        tat[i]=0;
+        for (j=0; j<n; j++){
+            wt[i]=wt[i]+bt[j];
+        }
+        tat[i]=wt[i]+bt[i];
+        awt=awt+wt[i];
+        atat=atat+tat[i];
+        printf("%d\t %d\t\t %d\t\t %d\n",p[i],bt[i],wt[i],tat[i]);
+    }
+    
+    awt=awt/n;
+    atat=atat/n;
+    printf("average waiting time = %f\n",awt);
+    printf("average turn around time =%f\n",atat);
+    getch();
+
+    
+}
